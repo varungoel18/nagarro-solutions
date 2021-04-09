0ba1f6c2fbf5728ad8f121fbdc064d8e8beacb2b
Varun Goel <52860777+varungoel18@users.noreply.github.com>
2021-04-09 12:51:40 +0530

    linear search using recursion

---
+++ linear search using recursion.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int lsearch(int arr[],int n,int key) {
+// base case
+if(n==0) {
+    return -1;
+}
+// recursive case
+if(arr[0]==key) {
+    return 0;
+}
+int i = lsearch(arr +1 ,n-1,key);
+if(i==-1) {
+    return -1;
+}
+return i+1;
+}
+int main() {
+int arr[] = {1,2,3,4,5,6,7,8,7,9};
+int n = sizeof(arr)/sizeof(int);
+int key;
+cin>>key;
+cout<<lsearch(arr,n,key);
+
+}
