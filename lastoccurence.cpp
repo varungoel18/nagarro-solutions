f4108c20a33d2f53da3759383c78d8ea90534364
Varun Goel <52860777+varungoel18@users.noreply.github.com>
2021-04-12 17:18:28 +0530

    first occurence using recursion

---
+++ lastoccurence.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int firstoccurence(int arr[],int n , int key) {
+    // base case
+if(n==0) {
+    return -1;
+}
+// recursive case
+if(arr[0]== key) {
+    return 0;
+}
+int i = firstoccurence(arr+1,n-1,key);
+if(i==-1) {
+    return -1;
+}
+    return i+1;
+}
+int main() {
+int arr[] = {1,2,3,4,5,7,8,7,9};
+int n = sizeof(arr)/sizeof(arr[0]);
+int key;
+cin>>key;
+cout<<firstoccurence(arr,n,key)<<endl;
+}
